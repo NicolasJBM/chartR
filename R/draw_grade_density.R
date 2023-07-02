a3278320786a7d91aaeb65fe4d28140f7b93c567
NicolasJBM <nicolas.jb.mangin@gmail.com>
2023-07-02 11:49:14 +0200

    REFACTOR: build comments and density on the same underlying structure.

--- R/draw_grade_density.R
+++ R/draw_grade_density.R
@@ -4,6 +4,8 @@
 #' @description Function drawing grade density and positioning the student relative to the mean and median.
 #' @param grades Tibble. List of grades of all students. Three necessary variables: student, points, grade.
 #' @param studentid Character. ID of the student.
+#' @param structure Tibble. Variables: threshold, status, color, and comment. Comment is not necessary.
+#' @param labels List. Names for axes and legends (for translations purposes)
 #' @return ggplot2 density graph.
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 annotate
@@ -16,24 +18,60 @@
 #' @export
 
 
-draw_grade_density <- function(grades, studentid){
+draw_grade_density <- function(
+    grades, studentid, structure,
+    labels = base::list(
+      x = "Grade",
+      y = "Proportion",
+      avg = "Mean",
+      med = "Median",
+      stu = "You"
+    )
+){
+  
+  x <- NULL
+  y <- NULL
   grade <- NULL
+  status <- NULL
+  
   avg <- base::mean(grades$grade)
   med <- stats::median(grades$grade)
   sg <- grades$grade[base::match(studentid, grades$student)]
-  maxx <- base::max(grades$points)
+  maxx <- base::max(grades$points)*1.1
   maxy <- stats::density(stats::na.omit(grades$grade))
   maxy <- base::max(maxy$y)*1.1
-  grades |>
-    ggplot2::ggplot(ggplot2::aes(x=grade)) +
-    ggplot2::geom_density() +
-    ggplot2::geom_vline(xintercept = avg, color = "blue", size = 1.25) +
-    ggplot2::geom_vline(xintercept = med, color = "forestgreen", size = 1.25) +
-    ggplot2::geom_vline(xintercept = sg, color = "red", size = 1.25) +
-    ggplot2::annotate("text", label = "Median", x = maxx*0.2, y = maxy*0.95, color = "forestgreen") +
-    ggplot2::annotate("text", label = "Average", x = maxx*0.2, y = maxy*0.90, color = "blue") +
-    ggplot2::annotate("text", label = "You", x = maxx*0.2, y = maxy*0.85, color = "red") +
+  
+  dens = base::data.frame(stats::density(grades$grade, n=2^10, adjust=1)[c("x","y")]) |>
+    dplyr::mutate(status = base::cut(x, breaks=c(
+      -Inf,
+      structure$threshold[1],
+      structure$threshold[2],
+      structure$threshold[3],
+      structure$threshold[4],
+      Inf
+    ))) |>
+    dplyr::group_by(status) |>
+    dplyr::mutate(prob = base::paste0(base::round(base::sum(y)*base::mean(base::diff(x))*100),"%"))
+  
+  # Assign colors to each level of section
+  col = stats::setNames(structure$color, base::levels(dens$status))
+  
+  ggplot2::ggplot(dens, ggplot2::aes(x, y, fill=status)) +
+    ggplot2::geom_area() +
+    ggplot2::geom_line(linewidth=1) +
+    ggplot2::scale_fill_manual(labels=structure$status, values = col) +
+    ggplot2::geom_vline(xintercept = avg, color = "blue", size = 1.25, lty = 2) +
+    ggplot2::geom_vline(xintercept = med, color = "skyblue3", size = 1.25, lty = 2) +
+    ggplot2::geom_vline(xintercept = sg, color = "purple", size = 1.25) +
+    ggplot2::annotate("text", label = labels$med, x = maxx*0.9, y = maxy*0.95, color = "blue") +
+    ggplot2::annotate("text", label = labels$avg, x = maxx*0.9, y = maxy*0.90, color = "skyblue3") +
+    ggplot2::annotate("text", label = labels$stu, x = maxx*0.9, y = maxy*0.85, color = "purple") +
     ggplot2::scale_x_continuous(breaks = base::seq(0,maxx,1)) +
-    ggplot2::ylab("Count") +
-    ggplot2::theme_light()
+    ggplot2::xlab(labels$x) +
+    ggplot2::ylab(labels$y) +
+    ggplot2::xlim(0, maxx) +
+    ggplot2::ylim(0, maxy) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "bottom")
+  
 }
